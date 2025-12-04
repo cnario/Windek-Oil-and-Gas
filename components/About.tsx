f1115c47e1f5448f9f3230bf27e716e28a3d73d1
CNARIO <cnariojoseph@gmail.com>
2025-12-04 13:34:54 +0100

    Fix About Image

--- components/About.tsx
+++ components/About.tsx
@@ -45,7 +45,7 @@ const About: React.FC = () => {
           <div className="order-1 lg:order-2 relative">
             <div className="relative z-10 rounded-lg overflow-hidden shadow-2xl shadow-slate-200">
               <img 
-                src="https://images.unsplash.com/photo-1621905252507-b35492cc74b4?q=80&w=1200&auto=format&fit=crop" 
+                src=""https://www.coastalcrestenergyltd.com/images/bg/9.jpg 
                 alt="Oil and Gas Engineers" 
                 className="w-full h-auto object-cover"
               />
